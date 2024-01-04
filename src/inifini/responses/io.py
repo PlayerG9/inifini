db9c03ea6e91cc17f71f1450694db6d30176e6cf
PlayerG9 <g9yout@gmail.com>
2024-01-04 21:53:11 +0100

    added block-size and auto-close parameters to IOResponse

--- src/inifini/responses/io.py
+++ src/inifini/responses/io.py
@@ -10,15 +10,18 @@ from ._kit import RangeSupport
 
 
 class IOResponse(ResponseBase, RangeSupport):
-    _stream = t.IO
+    _stream: t.IO
+    _blksize: t.Optional[int]
 
-    def __init__(self, stream: t.IO):
+    def __init__(self, stream: t.IO, *, blksize: t.Optional[int] = None, auto_close: bool = True):
         if not stream.readable():
             raise UnsupportedOperation("not readable")
         self._stream = stream
+        self._blksize = 8192 if blksize == 1 else blksize
+        self.auto_close = auto_close
 
     def __del__(self):
-        if not self._stream.closed:
+        if self.auto_close and hasattr(self._stream, 'closed') and not self._stream.closed:
             self._stream.close()
 
     @property
@@ -30,5 +33,16 @@ class IOResponse(ResponseBase, RangeSupport):
         self._stream.seek(pos)
         return end
 
+    # @cgi.util.FileWrapper inspired
+
     def __iter__(self):
-        yield from self._stream
+        if self._blksize is None:
+            yield from self._stream  # yield lines or so
+        else:
+            return self  # use __next__
+
+    def __next__(self):
+        data = self._stream.read(self._blksize)
+        if data:
+            return data
+        raise StopIteration
