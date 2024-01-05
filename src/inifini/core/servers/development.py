bb599ee3ecb412d6b96afb23d7596c13021661b4
PlayerG9 <g9yout@gmail.com>
2024-01-05 15:20:07 +0100

    removed jurigged code from development server

--- src/inifini/core/servers/development.py
+++ src/inifini/core/servers/development.py
@@ -3,39 +3,7 @@ r"""
 
 """
 from .production import ProductionServer
-try:
-    from better_exceptions import format_exception
-except ModuleNotFoundError:
-    from traceback import format_exception
-try:
-    import jurigged
-except ModuleNotFoundError:
-    pass
 
 
 class DevelopmentServer(ProductionServer):
-    def serve_forever(self, app) -> None:
-        registry = jurigged.register.Registry()
-        registry.auto_register(filter=self._get_filter())
-        # jurigged.watch
-        watcher = jurigged.Watcher(registry=registry)
-        watcher.start()
-        try:
-            super().serve_forever(app=app)
-        finally:
-            watcher.stop()
-
-    @staticmethod
-    def _get_filter():
-        import sys
-        import os.path as p
-        import fnmatch
-
-        root = getattr(sys.modules.get('__main__', None), '__file__', ".")
-        pattern = p.dirname(p.abspath(root)) + "/*.py"
-        print([root, pattern])
-
-        def allowed(filename: str) -> bool:
-            return fnmatch.fnmatch(filename, pattern)
-
-        return allowed
+    pass
